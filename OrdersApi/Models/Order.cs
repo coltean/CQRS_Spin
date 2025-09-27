8f0b6b6cf6154ac757ca923ddc8890d2cffd571f
Ciprian Oltean <Ciprian.Oltean@endava.com>
2025-09-27 17:48:31 +0300

    Add basic endpoints, add context and migrations

---
+++ OrdersApi/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace OrdersApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
